772d88db669c6f83daee0f944e26640419d46d0b
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-10-13 22:00:19 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> properties, params Expression<Func<T, Object>>[] includeProperties);
+
+    }
+}
