a236ad665d6d47a6f4979d369885a5c0066f6620
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-10-23 17:54:22 +0300

    Database Mapping Operations with Fluent API

--- ProgrammersBlog.Entities/Concrete/User.cs
+++ ProgrammersBlog.Entities/Concrete/User.cs
@@ -12,8 +12,8 @@ namespace ProgrammersBlog.Entities.Concrete
         public string FirstName {  get; set; }
         public string LastName {  get; set; }
         public string Email {  get; set; }
-        public byte[] PasswordHash { get; set; }
-        public string UserName { get; set; }
+		public string UserName { get; set; }
+		public byte[] PasswordHash { get; set; }
         public int RoleId {  get; set; }
         public Role Role { get; set; }
         public string Picture {  get; set; }
